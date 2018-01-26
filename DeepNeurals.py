090849d65c683592fb559faf9fedb604b0451e1d
Akash <akash.darknight@gmail.com>
2018-01-26 10:03:00 +0530

    Deep Neural networks

---
+++ DeepNeurals.py
@@ -0,0 +1,16 @@
+
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+from testCases_v2 import *
+from dnn_utils_v2 import sigmoid, sigmoid_backward, relu, relu_backward
+
+get_ipython().magic('matplotlib inline')
+plt.rcParams['figure.figsize'] = (5.0, 4.0)  # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+get_ipython().magic('load_ext autoreload')
+get_ipython().magic('autoreload 2')
+
+np.random.seed(1)
